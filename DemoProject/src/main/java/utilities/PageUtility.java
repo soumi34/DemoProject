f6428fe938e10fc8d81ea4aa580ce44add2e3b01
Gowrish <Gowrish@DESKTOP-5KK9CUH>
2026-03-29 07:14:50 +0530

    comitted succesfully

--- DemoProject/src/main/java/utilities/PageUtility.java
+++ DemoProject/src/main/java/utilities/PageUtility.java
@@ -1,27 +1,69 @@
 package utilities;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
-public class PageUtility//It is a helper class used to help page class 
-{
-	
-	public void dropDownVisibleText(WebElement element ,String text)
-	{
-		Select select = new Select(element);
-		select.selectByVisibleText(text);
-	}
-	public void dropDownSelectByValue(WebElement element ,String text)
-	{
-		Select select = new Select(element);
-		select.selectByValue(text);
-	}
-	public void dropDownSelectByIndex(WebElement element ,int index)
-	{
+public class PageUtility {
+	public void dropDownSelectByIndex(WebElement element, int index) {
 		Select select = new Select(element);
 		select.selectByIndex(index);
 	}
-	
-	
 
+	public void dropDownSelectByValue(WebElement element, String value) {
+		Select select = new Select(element);
+		select.selectByValue(value);
+	}
+	
+	public void dropDownSelectByVisibleText(WebElement element, String text) {
+		Select select = new Select(element);
+		select.selectByVisibleText(text);
+	}
+	
+	public void right_Click(WebDriver driver,WebElement input)
+	{
+		Actions a = new Actions(driver); 
+		a.contextClick(input).perform();
+	}
+	
+	public void mouse_Over(WebDriver driver,WebElement input)
+	{
+		Actions a = new Actions(driver); 
+		a.moveToElement(input).perform();
+	}
+	
+	public void double_Click(WebDriver driver,WebElement input)
+	{
+		Actions a = new Actions(driver); 
+		a.doubleClick(input).perform();
+	}
+	
+	public void click(WebDriver driver,WebElement input)
+	{
+		Actions a = new Actions(driver); 
+		a.click(input).perform();
+	}
+	
+	public void simpleAlerts(WebDriver driver)
+	{
+		Alert alert = driver.switchTo().alert(); // switch to alert
+		alert.accept();
+	}
+	
+	public void confirmationAlerts(WebDriver driver)
+	{
+		Alert alert = driver.switchTo().alert(); // switch to alert
+		alert.dismiss();
+	}
+	
+	public void promptAlerts(WebDriver driver)
+	{
+		Alert alert = driver.switchTo().alert(); // switch to alert
+		alert.sendKeys("Soumya");
+		alert.accept();
+	}
+	
 }
